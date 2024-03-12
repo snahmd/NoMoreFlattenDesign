5b0692c4b16f18ac66193bc94074344a8ee2da56
snahmd <sanahmed7@gmail.com>
2024-03-12 12:51:30 +0100

    Calculate Product Count in Categories

--- stock/serializers.py
+++ stock/serializers.py
@@ -2,6 +2,10 @@ from rest_framework import serializers
 from .models import Category, Brand, Product, Firm, Purchases, Sales    
 
 class CategorySerializer(serializers.ModelSerializer):
+    product_count = serializers.SerializerMethodField()
     class Meta:
         model = Category
         fields = '__all__'
+
+    def get_product_count(self, obj):
+        return Product.objects.filter(category_id=obj.id).count()    
