66fb8cb2e602e7ee54d3f90539f69667082b5225
snahmd <sanahmed7@gmail.com>
2024-03-18 14:10:10 +0100

    create search and filter

--- stock/views.py
+++ stock/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
 from .serializers import CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name']
+    filterset_fields = ['name']
